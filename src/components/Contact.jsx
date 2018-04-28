a90c83d231ab9d16073b90c9feaaeccca600cbca
jmazrimas <joe.mazrimas@gmail.com>
2018-04-28 17:13:49 -0500

    add hours/contact placeholder

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class Contact extends Component {
   render() {
     return (
-      <div className="services">
-        Contact
+      <div className="contact">
+        <div className="content con-padded flex center-center hours-contact">
+          <div>
+            Hours
+          </div>
+          <div>
+            Contact
+          </div>
+        </div>
       </div>
     );
   }
