7d129107e0653baadbbb0694b6921852e036586f
jmazrimas <joe.mazrimas@gmail.com>
2018-04-28 18:13:26 -0500

    add basic layout for hours/contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,12 +4,19 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact">
-        <div className="content con-padded flex center-center hours-contact">
+        <div className="content con-padded flex hours-contact">
           <div>
-            Hours
+            <div>OPENING HOURS</div>
+            <div>Mon - Fri: 11am - 8pm</div>
+            <div>Saturday: 9am - 7pm</div>
+            <div>Sunday: Closed</div>
           </div>
           <div>
-            Contact
+            <div>CONTACT ME</div>
+            <div>1258 N Milwaukee Ave</div>
+            <div>Chicago, 60622</div>
+            <div>groomingloungechicago@gmail.com</div>
+            <div>773-342-1258</div>
           </div>
         </div>
       </div>
