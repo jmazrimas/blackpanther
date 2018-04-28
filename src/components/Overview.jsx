f8df948be48e9baf272842a6b0713724c4a3fda8
jmazrimas <joe.mazrimas@gmail.com>
2018-04-28 18:07:42 -0500

    add circle image to about section

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import oldSchoolBarber from '../images/nick-karvounis-156112-unsplash.jpg';
+
 class Overview extends Component {
   render() {
     return (
       <div className="overview">
         <div className="content con-padded flex col center-center">
-          <div>Picture</div>
+          <div className="circle-image">
+            <img src={oldSchoolBarber} />
+          </div>
           <div className="summary-text">
             Grooming Lounge is an upscale, new-world barber shop that offers today’s progressive individual a haven where they can sit back, relax, and experience the world class art of grooming and services in a therapeutic club atmosphere. We carry a full line of hair care and shaving cream products including those from high-end brands, as well as select soaps and beard products. Our goal is to become a trusted destination where the clients in our community can come to refresh their minds and bodies, replenish their energies, and network with colleagues and friends.
           </div>
