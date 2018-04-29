7550f3fd704c1157ed629f1f7f8b4ff25026c3a1
jmazrimas <joe.mazrimas@gmail.com>
2018-04-29 07:51:48 -0500

    add GL logo to top

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import glWhite from '../images/GL_white.png';
 
 class Header extends Component {
   render() {
     return (
       <div className="app-header flex center-center">
         <div className="content flex col center-center">
-          <div className="main-title">
-            Grooming Lounge
+          <div className="header-container">
+            <img className="header-image" src={glWhite} />
           </div>
-          <div className="sub-title">Chicago</div>
         </div>
       </div>
     );
   }
 }
+
+// <div className="main-title">
+//   Grooming Lounge
+// </div>
+// <div className="sub-title">Chicago</div>
 
 export default Header;
