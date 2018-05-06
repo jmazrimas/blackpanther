05f1a880cf25a3efca50f8dde587757fd1727f07
jmazrimas <joe.mazrimas@gmail.com>
2018-05-06 12:46:34 -0500

    update packaged services to have includes key

--- src/components/PackagedServices.jsx
+++ src/components/PackagedServices.jsx
@@ -15,6 +15,9 @@ const packagedServiceList = [
     image: highball,
     descriptions: [
       "High and Tight Buzz Cut & Cleanse. No muss, no fuss."
+    ],
+    includes: [
+
     ]
   },
   {
@@ -23,8 +26,10 @@ const packagedServiceList = [
     image: theusual,
     descriptions: [
       "Traditional haircut.",
-      "Cleanse ⬧ Haircut ⬧ Styling",
       "Two week complimentary touch up",
+    ],
+    includes: [
+      "Cleanse", "Haircut", "Styling",
     ]
   },
   {
@@ -33,7 +38,9 @@ const packagedServiceList = [
     image: metropolitan,
     descriptions: [
       "Modern day classic for the professional who likes to keep it simple and stylish, with a little pampering.",
-      "Haircut  ⬧ Cleanse  ⬧ Paraffin hand treatment ⬧ Eyebrow  clean-up ⬧ Razor nape finishing  ⬧ Hot towel ⬧ Two week complimentary touch up",
+    ],
+    includes: [
+      "Haircut","Cleanse","Paraffin hand treatment", "Eyebrow  clean-up", "Razor nape finishing","Hot towel", "Two week complimentary touch up",
     ]
   },
   {
@@ -42,7 +49,9 @@ const packagedServiceList = [
     image: oldfashioned,
     descriptions: [
       "Being well groomed never goes out of style, turning Old School into New School.",
-      "Haircut ⬧ Cleanse ⬧ Eyebrow clean-up ⬧ Beard trim  ⬧ Razor nape finishing ⬧ Hot towel ⬧ Beard oil and balm treatment",
+    ],
+    includes: [
+      "Haircut", "Cleanse", "Eyebrow clean-up", "Beard trim","Razor nape finishing", "Hot towel", "Beard oil and balm treatment",
     ]
   },
   {
@@ -51,8 +60,10 @@ const packagedServiceList = [
     image: whiterussian,
     descriptions: [
       "Appear refreshed, confident and youthful. Camouflage the grey with a blending tint. Leaves hair natural looking and fades without any pigment change.",
-      "Cleanse ⬧ Haircut ⬧ Styling ⬧",
       "Two week complimentary touch up",
+    ],
+    includes: [
+      "Cleanse", "Haircut", "Styling"
     ]
   },
   {
@@ -61,8 +72,10 @@ const packagedServiceList = [
     image: hottoddy,
     descriptions: [
       "Rejuvenating men’s face treatment for the C-suite executive to the construction worker; every man should have a good facial regimen.",
-      "Cleansing ⬧ Toning ⬧ Exfoliation ⬧ Skin hydration ⬧ Hot towel ⬧ Cleanse ⬧ Haircut ⬧ Styling",
       "Two week complimentary touch up",
+    ],
+    includes: [
+      "Cleansing", "Toning", "Exfoliation", "Skin hydration", "Hot towel", "Cleanse", "Haircut", "Styling",
     ]
   },
   {
