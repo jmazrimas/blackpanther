bae84238eed72ba9d5e5d71d6d63cfc481d43b9d
jmazrimas <joe.mazrimas@gmail.com>
2018-05-06 12:39:57 -0500

    update pictures

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-import oldSchoolBarber from '../images/nick-karvounis-156112-unsplash.jpg';
+// import oldSchoolBarber from '../images/nick-karvounis-156112-unsplash.jpg';
+import about from '../images/about.jpg';
 
 class Overview extends Component {
   render() {
@@ -8,7 +9,7 @@ class Overview extends Component {
       <div className="overview">
         <div className="content con-padded flex col center-center">
           <div className="circle-image">
-            <img src={oldSchoolBarber} />
+            <img src={about} />
           </div>
           <div className="summary-text">
             Grooming Lounge is an upscale, new-world barber shop that offers today’s progressive individual a haven where they can sit back, relax, and experience the world class art of grooming and services in a therapeutic club atmosphere. We carry a full line of hair care and shaving cream products including those from high-end brands, as well as select soaps and beard products. Our goal is to become a trusted destination where the clients in our community can come to refresh their minds and bodies, replenish their energies, and network with colleagues and friends.
