20319e81dbc102ff163c07c2e9f9d82c72145786
jmazrimas <joe.mazrimas@gmail.com>
2020-03-21 12:17:47 -0500

    fix deploy script RE: aws profile

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,6 +3,6 @@ read -p "Deploy to PROD? (y/n)" yn
 if [ $yn = y ]
 then
   echo Deploying!
-  aws s3 sync build s3://groominglounge/blackpanther
-  aws s3 cp build/index.html s3://groominglounge/
+  aws s3 sync build s3://groominglounge/blackpanther --profile grooming
+  aws s3 cp build/index.html s3://groominglounge/ --profile grooming
 fi
