955d320021ea563a24cd82dc89873117638af082
jmazrimas <joe.mazrimas@gmail.com>
2020-05-11 19:51:11 -0500

    add policies

---
+++ src/components/Policies.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Policies extends Component {
+  render() {
+    return (
+      <div className="policies">
+        <div className="content con-padded flex col">
+          <div className="section-title">Policies</div>
+          <div>
+            Things
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Policies;
