c929a1ac11dcab914e778ef9072218c4a0b5c45e
Joseph Mazrimas <josephmazrimas@Josephs-MacBook-Pro.local>
2023-05-04 20:27:00 -0500

    change booking link to use https

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,7 +4,7 @@ class Book extends Component {
   render() {
     return (
       <div className="book-online">
-        <button onClick={() => window.open('http://book.insighthosted.com/$/Start/?ID=2FC6E1230053D9301F5AF1D3610CB560', '_blank', 'toolbar=no, menubar=no, width=1024, resize=no, scrollbar=no') }>
+        <button onClick={() => window.open('https://book.insightdns.com/$/Start/?ID=2FC6E1230053D9301F5AF1D3610CB560') }>
           book online
         </button>
       </div>
